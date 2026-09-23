a102c8bce79cc3f6e06d2ea0c0c68e3cc05c7528
kingazi8989-arch <kingazi8989@gmail.com>
2026-09-23 22:08:22 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -17,4 +18,6 @@ def home():
     </html>
     """
 
-app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
