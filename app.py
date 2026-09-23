bfe76851d6f93893b86bbe6306ab1da344a24ca2
kingazi8989-arch <kingazi8989@gmail.com>
2026-09-23 20:53:55 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>My Panel</title>
+    </head>
+    <body style="font-family:sans-serif; text-align:center;">
+        <h1>My Panel</h1>
+        <p>Panel is running successfully ✅</p>
+    </body>
+    </html>
+    """
+
+app.run(host="0.0.0.0", port=8080)
